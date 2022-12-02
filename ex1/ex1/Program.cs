1f1621d2c8ed39b254b5714377d031ad9a4e2646
Stefane Gomes <80134693+stefanegomess@users.noreply.github.com>
2022-12-01 22:36:55 -0300

    Add files via upload

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça um programa que receba dois números, calcule e
+            //mostre a subtração do primeiro número pelo segundo.
+
+            float A, B, subtracao;
+
+            Console.WriteLine("Digite 2 números: ");
+
+            A = float.Parse(Console.ReadLine());
+            B = float.Parse(Console.ReadLine());
+
+            subtracao = A - B;
+
+            Console.WriteLine("A subtração do primeiro número pelo segundo é: " +subtracao);
+        }
+    }
+}
